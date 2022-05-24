93de7606a6c6cab1dc22670bf0f6f691cc366f02
williambeach <intratech.cs@icloud.com>
2022-05-24 14:13:15 -0400

    modify button classification for event listener logic. modify css tags to reflect updated html classifications. add number handling in js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,17 @@
 const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('.num');
+const display = document.querySelector('#display');
+const back = document.querySelector('#backspace');
+const clear = document.querySelector('#allclear');
+const decimal = document.querySelector('#decimal');
+let firstNum = "";
+let secondNum = "";
+
+
+function removeClickEffect(e) {
+    this.classList.remove('clickEffect');
+}
+
 function hoverEffect() {
     for (let i=0; i<buttons.length;i++) {
         buttons[i].addEventListener('mouseover', () => {
@@ -10,17 +23,86 @@ function hoverEffect() {
     }
 }
 
-function removeTransition(e) {
-    this.classList.remove('buttonClick');
+function clickEffect() {
+    for (let i=0; i<buttons.length;i++) {
+        buttons[i].addEventListener('click', ()=> {
+            buttons[i].classList.add("clickEffect");
+        });
+        buttons[i].addEventListener('transitionend', removeClickEffect);
+    }
 }
 
-for (let i=0; i<buttons.length;i++) {
-    buttons[i].addEventListener('click', ()=> {
-        buttons[i].classList.add("buttonClick");
+function displayNumbers() {
+    for (let i=0;i<numbers.length;i++) {
+        numbers[i].addEventListener('click', ()=> {
+            if (display.textContent.length < 16) {
+                display.textContent += numbers[i].value;
+            }
+        });
+    }
+}
+
+function backSpace() {
+    back.addEventListener('click', ()=> {
+        display.textContent = display.textContent.slice(0,-1);
+        firstNum = firstNum.slice(0,-1);
     });
-    buttons[i].addEventListener('transitionend', removeTransition);
+}
+
+function allClear() {
+    clear.addEventListener('click', ()=> {
+        display.textContent = "";
+        firstNum = "";
+    });
+}
+
+function getFirstNumber() {
+    for (let i=0; i<numbers.length;i++) {
+        numbers[i].addEventListener('click', ()=> {
+            if (firstNum.length < 16) {
+                firstNum += numbers[i].value;
+            }
+            console.log(firstNum);
+        });
+    }
+}
+
+function addDecimal() {
+    decimal.addEventListener('click', ()=> {
+        if (!firstNum.includes('.')) {
+            display.textContent += decimal.value;
+            firstNum += decimal.value;
+        }
+    });
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+addDecimal();
+displayNumbers();
+allClear();
+backSpace();
 hoverEffect();
+clickEffect();
+getFirstNumber();
